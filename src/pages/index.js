ec2bdad2fdddc530d1ac706dc6bb17003a06cf16
Hunter Caron <huntercaron@mac.com>
2018-02-22 10:16:05 -0500

    changin from higher order component to normal component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
-import Button from '../components/Button'
-import 'intersection-observer'
+import ContentBlock from '../components/ContentBlockNormal'
 
 // styled components
 const Container = styled.div`
@@ -16,7 +15,7 @@ const Subtitle = styled.div`
 const List = styled.div`
 `
 
-const BlockContainer = styled.div`
+const BlockContainer = styled(ContentBlock)`
   position: relative;
   height: 200vh;
   border: 1px solid red;
@@ -29,51 +28,6 @@ const StickyLayer = styled.div`
   position: sticky;
 `;
 
-const StickyDetector = styled.div`
-  position: absolute;
-  left: 0;
-  right: 0;
-  /* visibility: hidden; */
-  border: 1px solid pink;
-`
-
-const TopStickyDetector = StickyDetector.extend`
-  height: 40px;
-  top: -40px;
-`;
-
-const BottomStickyDetector = StickyDetector.extend`
-  height: 80px;
-  bottom: 0;
-`;
-
-// components
-function ContentBlock(props) {
-  return (
-    <div />
-  )
-}
-
-function createObserver() {
-
-}
-
-function withContentBlockEvents(WrapperComponent) {
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-
-      this.state = { active: false, index: 0 || props.index };
-    }
-
-    observeHeader = () => {};
-
-    observeFooter = () => {};
-
-    componentDidMount() {}
-  };
-}
-
 // page component
 export default class IndexPage extends React.Component {
   constructor(props) {
@@ -82,105 +36,32 @@ export default class IndexPage extends React.Component {
     this.state = {
       activeBlock: 0
     }
-
-    this.topDetectors = [];
-    this.bottomDetectors = [];
-  }
-  
-
-  addSentinels = (container, className) => {
-    return Array.from(container.querySelectorAll('.sticky')).map(el => {
-      const sentinel = document.createElement('div');
-      sentinel.classList.add('sticky_sentinel', className);
-      return el.parentElement.appendChild(sentinel);
-    });
-  }
-  
-  fireEvent = (stuck, target) => {
-    this.setState({ activeBlock: target.dataset.num });
-    const e = new CustomEvent('sticky-change', {detail: {stuck, target}});
-    document.dispatchEvent(e);
   }
 
-  observeHeaders = (container) => {
-    const observer = new IntersectionObserver((records, observer) => {
-      for (const record of records) {
-        const targetInfo = record.boundingClientRect;
-        const stickyTarget = record.target.parentElement.querySelector(".sticky");
-        const rootBoundsInfo = record.rootBounds;
-
-        // Started sticking.
-        if (targetInfo.bottom < rootBoundsInfo.top) {
-          this.fireEvent(true, stickyTarget);
-        }
-
-        // Stopped sticking.
-        if (targetInfo.bottom >= rootBoundsInfo.top && targetInfo.bottom < rootBoundsInfo.bottom) {
-          this.fireEvent(false, stickyTarget);
-        }
-      }
-    }, {threshold: [0], root: null});
-
-    this.topDetectors.forEach(el => observer.observe(el));
-  }
-
-  observeFooters = (container) => {
-    const observer = new IntersectionObserver((records, observer) => {
-      for (const record of records) {
-        const targetInfo = record.boundingClientRect;
-        const stickyTarget = record.target.parentElement.querySelector('.sticky');
-        const rootBoundsInfo = record.rootBounds;
-        const ratio = record.intersectionRatio;
-
-        // Started sticking.
-        if (targetInfo.bottom > rootBoundsInfo.top && ratio === 1) {
-          this.fireEvent(true, stickyTarget);
-        }
-
-        // Stopped sticking.
-        if (targetInfo.top < rootBoundsInfo.top &&
-          targetInfo.bottom < rootBoundsInfo.bottom) {
-          this.fireEvent(false, stickyTarget);
-        }
-      }
-    }, {
-      threshold: [1],
-      root: null
-    });
-
-    this.bottomDetectors.forEach(el => observer.observe(el));
-  }
-  
-  observeStickyLayerChanges = (container) => {
-    this.observeHeaders(container);
-    this.observeFooters(container);
-  }
-
-  componentDidMount() {
-    this.observeStickyLayerChanges(this.container);
+  handleActiveChange = (index) => {
+    this.setState({
+      activeBlock: index
+    })
   }
 
   render() {
     const pages = this.props.data.allMarkdownRemark.edges;
+
     return (
       <Container>
         <Subtitle>dynamic pages:</Subtitle>
         
         <List>
           {pages.map( ({ node: page }, i) => (
-            <BlockContainer key={page.id}>
-              <TopStickyDetector innerRef={el => this.topDetectors[i] = el}/>
+            <BlockContainer key={page.id} index={i} handleActiveChange={this.handleActiveChange}>
 
-              <StickyLayer className="sticky" data-num={i}>
+              <StickyLayer>
                 THIS: {i}, ACTIVE: {this.state.activeBlock}
               </StickyLayer>
 
-              <BottomStickyDetector innerRef={el => this.bottomDetectors[i] = el}/>
             </BlockContainer>
           ))}
         </List>
-
-        <Button text="this is a button component" />
 
       </Container>
     )
