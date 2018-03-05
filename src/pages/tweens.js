8edb5e6040c7bd9b5c7761b345c097c5cc60dbc6
Hunter Caron <huntercaron@mac.com>
2018-03-04 19:34:43 -0500

    speeding it up

--- src/pages/tweens.js
+++ src/pages/tweens.js
@@ -49,6 +49,7 @@ export default class App extends React.Component {
 
   lastProgress = 0;
 
+
   setupAnimation = () => {
     this.tl = new TimelineMax();
     this.tl.to(this.square, 0.5, { rotation: 180, scale: 2, ease: Power1.easeInOut });
@@ -61,7 +62,7 @@ export default class App extends React.Component {
     let percent = this.animDuration / 100 * this.state.progress;
     let progressDiff = (Math.abs(this.state.progress - this.lastProgress)+1)*0.01;
 
-    this.tl.tweenTo(percent).duration(0.25+progressDiff);
+    this.tl.tweenTo(percent).duration(0.25+0.5*progressDiff);
     this.lastProgress = this.state.progress;    
   }
 
